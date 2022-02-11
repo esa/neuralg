aa81dbe3e957c3289ac95ea9b4e5257ad451ef62
Tove Ågren <98808889+toveag@users.noreply.github.com>
2022-02-11 17:53:56 +0100

    n_neurons

--- MLP.py
+++ MLP.py
@@ -4,7 +4,7 @@ import torch.nn.functional as F
 import numpy as np
 
 class MLP(nn.Module):
-    def __init__(self,matrix_dimension,hidden_layers, activation = nn.ReLU(), output_activation = nn.Tanh()):
+    def __init__(self,matrix_dimension,hidden_layers, n_neurons activation = nn.ReLU(), output_activation = nn.Tanh()):
         super(MLP, self).__init__()
         self.matrix_dimension = matrix_dimension
         self.flatten = nn.Flatten(start_dim=2)
@@ -12,11 +12,11 @@ class MLP(nn.Module):
         self.net = []
         
         #Input layer
-        self.net.append(nn.Linear(self.matrix_dimension**2,self.matrix_dimension**2))
+        self.net.append(nn.Linear(self.matrix_dimension**2,n_neurons))
         self.net.append(nn.ReLU())
         #Hidden layers
         for i in range(hidden_layers-1):
-            self.net.append(nn.Linear(self.matrix_dimension**2,self.matrix_dimension**2))
+            self.net.append(nn.Linear(self.matrix_dimension**2,n_neurons))
             self.net.append(activation)
         #Output layer 
         self.net.append(nn.Linear(self.matrix_dimension**2,self.matrix_dimension**2))
