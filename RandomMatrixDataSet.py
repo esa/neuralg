corrected scaling in eigval distributions

RandomMatrixDataSet.py
@@ -63,13 +63,13 @@ class RandomMatrixDataSet:
         self.X_Hess = None
         
     def from_rand(self, r1 = -10, r2 = 10): 
-        self.X = (r2-r1)*torch.rand(self.N,1,self.d,self.d)
+        self.X = (r1-r2)*torch.rand(self.N,1,self.d,self.d) + r2
     
     def from_randn(self):
         self.X = torch.randn(self.N,1,self.d,self.d)
     
     def from_dist(self, dist):
-        self.X = SymmetricMatrix(self.N,self.d, dist = dist).X
+        self.X = SymmetricMatrix(N = self.N, d = self.d, dist = dist).X
         
     def compute_labels(self):
         if self.operation == "lanczos":
@@ -149,7 +149,7 @@ class SymmetricMatrix():
     def __init__(self,N,d = 5, sigma= 10/math.sqrt(3), dist = "gaussian"):
         self.N = N
         self.d = d
-        self.sigma = sigma
+        self.sigma = math.sqrt(self.d)*sigma #Std of Wigner matrices 
         self.dist = dist
         self.X = None
         self.from_distribution(dist = self.dist)
@@ -163,15 +163,15 @@ class SymmetricMatrix():
         P = torch.linalg.eigh(M)[1]
         #Sample new eigenvalues 
         if self.dist == "gaussian":
-            x = self.sigma * torch.randn(self.N, self.d, 1)
+            x = self.sigma*torch.randn(self.N, self.d, 1)
         elif self.dist == "uniform":
-            x = 20*torch.rand(self.N, self.d, 1)
+            x = -2*math.sqrt(self.d)*10*torch.rand(self.N, self.d, 1) + math.sqrt(self.d)*10
         elif self.dist == "laplace":
             m = torch.distributions.Laplace(torch.tensor([0.0]), torch.tensor([self.sigma/math.sqrt(2)]))
             x = m.rsample(sample_shape=torch.Size([self.N,self.d]))
         diag = torch.eye(x.shape[1]) * x[:, None]
         #Finally, construct the resulting matrix batch with specified eigenvalues
-        self.X = torch.matmul(torch.matmul(M, diag), torch.transpose(M,2,3))
+        self.X = torch.matmul(torch.matmul(P, diag), torch.transpose(P,2,3))
 
 
 class EigenMatrix():

train.py
@@ -12,7 +12,7 @@ def train_on_batch(batch, model, loss_fcn, optimizer, scheduler=None):
         pred = model(batch.X)
     if loss_fcn == eigval_error or loss_fcn == eigval_L1:
         batch.compute_labels()
-        sorted_eigvals = torch.sort(torch.real(batch.Y[0]),2)[0] #For full eigenvalue decomposition
+        sorted_eigvals = torch.sort(torch.real(batch.Y[0]),2)[0]  #For all eigenvalues
         loss = loss_fcn(pred, sorted_eigvals)
     elif loss_fcn == max_eigval_error: 
         batch.compute_labels()
@@ -62,7 +62,7 @@ def run_training(k,model,loss_fcn,optimizer,matrix_parameters, scheduler = None,
     
     sizes = [5,6,7,8,9,10]
     
-    distributions = ["gaussian","uniform","laplace"]
+    distributions = ["gaussian","laplace","uniform"]
     for e in range(1,epoch+1):
         for i in range(k):
 