16749d171a2e7fec4cfc48679c7a824349c29a2f
tove <toveag@kth.se>
2022-03-17 13:41:44 +0100

    eig operation and clearing of models

--- neuralg/__init__.py
+++ neuralg/__init__.py
@@ -3,16 +3,10 @@ from loguru import logger
 
 # Add exposed features here
 from .ops.eig import eig
-from .utils.load_default_cfg import load_default_cfg
-from .scripts.train_models import train_models
 from .utils.ModelHandler import ModelHandler
 from .utils.set_log_level import set_log_level
 from .utils.set_precision import set_precision
-from .utils.print_cfg import print_cfg
-from .plots.plot_loss_logs import plot_loss_logs
-from .plots.plot_eigval_results import plot_eigval_results
-from .training.save_run import save_run
-from .evaluation.evaluate import evaluate
+from .utils.clear_loaded_models import clear_loaded_models
 
 set_log_level("INFO")
 
@@ -22,22 +16,11 @@ if "TORCH_DEVICE" not in os.environ:
 
 logger.info(f"Initialized neuralg for {os.environ['TORCH_DEVICE']}")
 
-# Set precision (and potentially GPU)
+# Set default precision
 set_precision()
 
 # Initialize global model handler
 neuralg_ModelHandler = ModelHandler()
 
-# Potentially in the final module not all of these should be imported,
-# but I have it right now for the notebook.
-__all__ = [
-    "eig"
-    #     ,"evaluate",
-    #     "load_default_cfg",
-    #     "load_model",
-    #     "plot_loss_logs",
-    #     "plot_eigval_results",
-    #     "print_cfg",
-    #     "save_run",
-    #     "train_models"
-]
+
+__all__ = ["eig", "set_precision", "set_log_level", "clear_loaded_models"]
---
+++ neuralg/tests/eig_test.py
@@ -0,0 +1,88 @@
+import torch
+import numpy as np
+from dotmap import DotMap
+from itertools import compress
+
+from ..ops.eig import eig
+from ..utils.constants import NEURALG_MIN_MATRIX_SIZE, NEURALG_MAX_MATRIX_SIZE
+from ..evaluation.evaluate_model import evaluate_model
+from ..evaluation.compute_accuracy import compute_accuracy
+
+
+def test_eig():
+    """Tests if the eig operation works for all supported sizes and is within defined errors bounds. 
+    The test set is sampled with a fixed seed. 
+    """
+    supported_sizes = np.arange(NEURALG_MIN_MATRIX_SIZE, NEURALG_MAX_MATRIX_SIZE + 1)
+
+    # Initially, we only test performance on Wigner matrices with eigvals with variance as in training.
+    # Potentially, I think one will want to expand the error bounds to include more diverse and out-of training examples,
+    # depending on the purpose
+    test_parameters = {"N": 10000, "operation": "eig", "wigner": True}
+
+    # These are set pretty high right now
+    tol = 0.15  # Preliminary tolerace
+    error_bound = 0.95  # Percentage of matrices we require to lie within the tolerance
+    results = DotMap()
+    # Track which models fails the error bound
+    results.failed = []
+    results.accuracy = []
+    for matrix_size in supported_sizes:
+        test_parameters["d"] = matrix_size
+        # Make sure that it supports different types of input e.g. in and out of batch mode
+        test_matrix = torch.rand(matrix_size, matrix_size)
+        test_batch = torch.rand(2, matrix_size, matrix_size)
+        out1, out2 = eig(test_matrix), eig(test_batch)
+
+        assert out1 is not None
+        assert out1.__class__.__name__ == "Tensor"
+        assert out1.shape == torch.Size([matrix_size])
+
+        assert out2 is not None
+        assert out2.__class__.__name__ == "Tensor"
+        assert out2.shape == torch.Size([2, matrix_size])
+
+        # Should also handle invalid input by throwing value errors
+        try:
+            out = eig(torch.rand(3, 4))
+        except ValueError:
+            None
+        try:
+            out = eig(torch.rand(3))
+        except ValueError:
+            None
+
+        # This is if we want it to be numpy compatible, it actually seemed to cause some trouble
+        # when I tried to fix it. But did not want to spend time on it at this stage
+
+        # test_numpy = np.random.rand(matrix_size, matrix_size)
+        # out3 = eig(test_numpy)
+        # assert out3 is not None
+        # assert out3.__class__.__name__ == "Tensor"
+        # assert out3.shape == torch.Size([matrix_size])
+
+        # # Should also work in batch mode but with numpy
+        # test_numpy = np.random.rand(2, 1, matrix_size, matrix_size)
+        # out4 = eig(test_numpy)
+        # assert out4 is not None
+        # assert out4.__class__.__name__ == "Tensor"
+        #  # assert out4.shape == torch.Size([2, 1, matrix_size])
+
+        # Test that errors are within the bound
+        error = evaluate_model(eig, test_parameters)
+        accuracy = compute_accuracy(tol, error)
+        results.accuracy.append(round(accuracy.item(), 3))
+        results.failed.append(bool(accuracy - error_bound < 0))
+
+    # Chech what,if any, models failed the requirements
+    assert (
+        sum(results.failed) == 0
+    ), "Error bound not reached: Failed for matrix sizes {} with accuracy {}, resepectively".format(
+        list(compress(supported_sizes, results.failed)),
+        list(compress(results.accuracy, results.failed)),
+    )
+
+
+if __name__ == "__main__":
+    test_eig()
+
