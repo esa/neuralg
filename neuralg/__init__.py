fc98465b5cdef1361654c9158d39bb3b72a4e630
tove <toveag@kth.se>
2022-03-28 22:03:42 +0200

    updated examplte problem and cuda setup

--- neuralg/__init__.py
+++ neuralg/__init__.py
@@ -7,6 +7,7 @@ from .utils.ModelHandler import ModelHandler
 from .utils.set_log_level import set_log_level
 from .utils.set_precision import set_precision
 from .utils.clear_loaded_models import clear_loaded_models
+from .utils.set_up_torch import set_up_torch
 
 set_log_level("INFO")
 
@@ -16,11 +17,17 @@ if "TORCH_DEVICE" not in os.environ:
 
 logger.info(f"Initialized neuralg for {os.environ['TORCH_DEVICE']}")
 
-# Set default precision ti float32 and uses CUDA If available
+# Set default precision to float32 and uses CUDA if initialized
 set_precision()
 
 # Initialize global model handler
 neuralg_ModelHandler = ModelHandler()
 
 
-__all__ = ["eig", "set_precision", "set_log_level", "clear_loaded_models"]
+__all__ = [
+    "eig",
+    "clear_loaded_models",
+    "set_precision",
+    "set_log_level",
+    "set_up_torch",
+]
