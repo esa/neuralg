00c8c2d24be2164701c1fe6d538644b059da8ae9
tove <toveag@kth.se>
2022-03-30 09:31:01 +0200

    small comment fix

--- neuralg/utils/set_up_torch.py
+++ neuralg/utils/set_up_torch.py
@@ -1,14 +1,12 @@
-from loguru import logger
 from .set_precision import set_precision
 from .enable_cuda import enable_cuda
 
 
 def set_up_torch(data_type=None, torch_enable_cuda=True):
-    """Configure a numerical backend for torchquad.
-    Calls torchquad.enable_cuda unless torch_enable_cuda is False.
-    If a data type is passed, set the default floating point precision with torchquad.set_precision.
+    """ Configure device and potentially precision for neuralg backend.
+    Calls neuralg.enable_cuda unless torch_enable_cuda is False.
+    If a data type is passed, set the default floating point precision with neuralg.set_precision.
     Args:
-
         data_type ("float32", "float64" or None, optional): Data type which is passed to set_precision. If None, do not call set_precision except if CUDA is enabled for torch. Defaults to None.
         torch_enable_cuda (Bool, optional): If True and backend is "torch", call enable_cuda. Defaults to True.
     """
