17a3a99007f89a11052f92b82363664cb7976997
tove <toveag@kth.se>
2022-04-12 07:28:21 +0200

    elaborated setup file for packaging

--- setup.py
+++ setup.py
@@ -5,17 +5,31 @@ https://github.com/pypa/sampleproject
 """
 
 # Always prefer setuptools over distutils
-from setuptools import setup, find_packages
+from setuptools import setup
 
 setup(
     name="neuralg",
     version="0.0.1",
+    description="Package providing torch-based neural network approximators of linear algebra operations.",
+    long_description=open("README.md").read(),
+    long_description_content_type="text/markdown",
+    # url="https://github.com/esa/neuralg",
+    author="ESA Advanced Concepts Team",
+    # author_email="toveag@kth.se",
     install_requires=[
         "matplotlib>=3.3.3",
         "torch==1.9.0",
         "loguru>=0.5.3",
         "dotmap>=1.3.24",
         "toml>=0.10.2",
+    ],
+    classifiers=[
+        # "Development Status :: 3 - Alpha", //Not sure what this should be
+        "Intended Audience :: Developers",
+        "Intended Audience :: Science/Research",
+        "Topic :: Scientific/Engineering :: Mathematics",
+        "License :: OSI Approved :: GNU General Public License v3 (GPLv3)",
+        "Programming Language :: Python :: 3.8",
     ],
     packages=[
         "neuralg",
@@ -26,7 +40,9 @@ setup(
         "neuralg.training",
         "neuralg.plots",
         "neuralg.utils",
+        "neuralg.evaluation",
     ],
     python_requires=">=3.8, <4",
+    # project_urls={
+    #    "Source": "https://github.com/esa/neuralg/"},
 )
-
