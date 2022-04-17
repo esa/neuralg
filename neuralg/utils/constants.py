c597fb75ada2ec1e2b95f6f57db92f90aa65bfcf
tove <toveag@kth.se>
2022-04-17 15:09:36 +0200

    workflow pt3 :) and added model

--- neuralg/utils/constants.py
+++ neuralg/utils/constants.py
@@ -8,7 +8,7 @@ NEURALG_MATRIX_SIZES.eig.sym.upper_bound = 20
 NEURALG_MATRIX_SIZES.eig.real.lower_bound = 3
 NEURALG_MATRIX_SIZES.eig.real.upper_bound = 10
 
-NEURALG_MATRIX_SIZES.eig.complex.lower_bound = 3
+NEURALG_MATRIX_SIZES.eig.complex.lower_bound = 2
 NEURALG_MATRIX_SIZES.eig.complex.upper_bound = 5
 
 NEURALG_MATRIX_SIZES.svd.lower_bound = 3
