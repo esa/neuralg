fde4f5baedbd3c5d8b5b69e7320f3ded2464fb92
tove <toveag@kth.se>
2022-04-18 13:12:01 +0200

    added real eigval2

--- neuralg/utils/constants.py
+++ neuralg/utils/constants.py
@@ -5,7 +5,7 @@ NEURALG_MATRIX_SIZES = DotMap()
 NEURALG_MATRIX_SIZES.eig.sym.lower_bound = 2
 NEURALG_MATRIX_SIZES.eig.sym.upper_bound = 20
 
-NEURALG_MATRIX_SIZES.eig.real.lower_bound = 3
+NEURALG_MATRIX_SIZES.eig.real.lower_bound = 2
 NEURALG_MATRIX_SIZES.eig.real.upper_bound = 10
 
 NEURALG_MATRIX_SIZES.eig.complex.lower_bound = 2
