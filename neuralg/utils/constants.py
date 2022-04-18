e2feaaa21cf01b119bcf20ee075178c97cbb8998
tove <toveag@kth.se>
2022-04-18 14:27:32 +0200

    models from branch merge

--- neuralg/utils/constants.py
+++ neuralg/utils/constants.py
@@ -12,7 +12,7 @@ NEURALG_MATRIX_SIZES.eig.complex.lower_bound = 3
 NEURALG_MATRIX_SIZES.eig.complex.upper_bound = 5
 
 NEURALG_MATRIX_SIZES.svd.lower_bound = 3
-NEURALG_MATRIX_SIZES.svd.upper_bound = 10
+NEURALG_MATRIX_SIZES.svd.upper_bound = 20
 
 
 NEURALG_SUPPORTED_OPERATIONS = ["eig", "svd"]
